92f279bf84b85457adaeea7ae2c8f89296b70cda
Hermes <adanhermes23@gmail.com>
2023-01-31 13:04:50 -0600

    fix analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,10 @@ import { AppContextProvider } from '../contexts/AppContext'
 import { UserProvider } from '@auth0/nextjs-auth0'
 import { UserProvider as AtlasUserProvider} from '../contexts/UserContext'
 import CustomCursor from '../components/atoms/CustomCursor/cat-cursor'
-import GoogleAnalytics from '../components/atoms/googleanalytics/GoogleAnalytics' 
 import IndexPage from '../components/atoms/IndexPage/index-page'
+import Script from 'next/script'
+
+const GA_TRACKING_ID = `${process.env.NEXT_PUBLIC_ANALYTICS_ID}`
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -46,7 +48,24 @@ export default function App({ Component, pageProps }: AppProps) {
           <Layout>
             < IndexPage />
             <CustomCursor />
-            <GoogleAnalytics />
+                        <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                strategy="afterInteractive"
+            />
+            <Script
+                id="ga-init"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: ` 
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${GA_TRACKING_ID}', {
+                            page_path: window.location.pathname,
+                        });
+                    `,
+                }}
+            />
           <Component {...pageProps} />
         </Layout>
         </AppContextProvider> 
