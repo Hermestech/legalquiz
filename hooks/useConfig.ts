1fd3109513d728ec50fca4606499a283022cc563
Hermes <adanhermes23@gmail.com>
2023-02-06 21:54:39 -0600

    add write process

--- hooks/useConfig.ts
+++ hooks/useConfig.ts
@@ -3,7 +3,7 @@ import { useLocalStorage } from "./useLocalStorage";
 export const useWriteKey = () => 
     useLocalStorage(
         'segment_write_key',
-        process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY_DEV
+        process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY
     )
 
 export const useCDNUrl = () =>
