8490dc5b333d54e086b2c4774d2f8a3da9ce9e3a
Hermes <adanhermes23@gmail.com>
2023-02-24 22:00:48 -0600

    adding questions

--- components/Layouts/QuestionariesLayout/questionaries-layout.tsx
+++ components/Layouts/QuestionariesLayout/questionaries-layout.tsx
@@ -75,39 +75,3 @@ export default function QuestionariesLayout({ questionaries }: QuestionariesLayo
 
     )
 }
-
-    //     <Grid container spacing={4}  sx={{width:'100%', height:'100%' }}>
-    //         {questionaries.map((questionary) => (
-    //             <Grid item xs={12} sm={6} md={4} key={questionary.sys.id}>
-    //                 <Card
-    //                     onClick={() => {Router.push(`/${questionary.sys.id}`)}}
-    //                 >
-    //                           <CardHeader
-    //     avatar={
-    //                             <Avatar
-    //                             aria-label="recipe"
-    //                             src={'/owl-lawyer.png'}
-    //                             />
-    //     }
-
-    //   />
-    //                     <CardContent>
-    //                     <Typography variant="h5" component="h2">
-    //                             {questionary.title}
-    //                         </Typography>
-    //                         <Typography variant="body2" color="text.secondary">
-    //                             {questionary.description}
-    //                         </Typography>
-    //                     </CardContent>
-    //                     <Divider />
-    //                     <CardActionArea
-    //                         sx={{ padding:'1rem' }}
-    //                     >
-    //                         <Typography variant="body2" color="text.secondary">
-    //                             {questionary.difficulty}
-    //                         </Typography>
-    //                     </CardActionArea>
-    //                 </Card>
-    //             </Grid>
-    //         ))}
-    //     </Grid>
